2d52abed38f811810b12448a410e902581106d9e
Farukh ijaz <ahmadelectricaltraders@gmail.com>
2024-10-07 20:00:09 +0500

    vitalstat_backend

--- VitalStatsAI_Web/settings.py
+++ VitalStatsAI_Web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7s%$%bow-tl5@3&%$ku97egvo$0mxe*7#wo=+$6m$wak=knw-k
 DEBUG = True
 
 AUTH_USER_MODEL = 'Admin_app.CustomUser'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,7 +126,7 @@ STATICFILES_DIRS = [
 
 # For production (collect static files)
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
