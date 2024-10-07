fc40154193650006605840fcf11c8a1ed14ce1a7
Farukh ijaz <ahmadelectricaltraders@gmail.com>
2024-10-07 19:23:10 +0500

    static file error fix

--- VitalStatsAI_Web/settings.py
+++ VitalStatsAI_Web/settings.py
@@ -116,8 +116,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
+
+# For development environment
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# For production (collect static files)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
